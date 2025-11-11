ecb5d28787c0035995fd8803a0de04daada3f09b
Daorza <daokun04@gmail.com>
2025-11-11 08:03:31 +0700

    adding style ChooseRegister

--- App/Views/Auth/ChooseRegister.php
+++ App/Views/Auth/ChooseRegister.php
@@ -11,7 +11,7 @@
         <div class="mt-9">
           <div class="mb-4 w-full">
             <a class="px-8 py-2 bg-primary text-white capitalize text-md font-medium rounded-md shadow cursor-pointer hover:bg-emerald-700 hover:ring-2 hover:ring-emerald-500 hover:ring-offset-2 active:bg-emerald-700 active:ring-2 active:ring-emerald-500 active:ring-offset-2 transition-all"
-              href="/register/dosen">Registrasi sebagai Dosen</a>
+              href="/register/dosen">Registrasi sebagai Dosen atau Tendik</a>
           </div>
 
           <div class="mb-4 w-full">
@@ -24,3 +24,6 @@
           </p>
         </div>
       </div>
+    </div>
+  </div>
+</div>
