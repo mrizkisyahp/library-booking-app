d5bfe30906433bb441ec6648a0788ed0a2b65266
Mohammad Rizki Syahputra <mrizkisyah27@gmail.com>
2025-11-28 08:37:54 +0700

    Fix conflict merger

--- App/Views/layouts/main.php
+++ App/Views/layouts/main.php
@@ -171,7 +171,7 @@ $user = App::$app->user;
                         </span>
                     </a>
                     <form action="/logout" method="post">
-                       <?= csrf_field() ?>
+                        <?= csrf_field() ?>
                         <button type="submit"
                             class="flex items-center gap-4 p-3 w-full rounded-xl hover:bg-red-600 transition-all mb-4">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-icon lucide-circle size-6 shrink-0"><circle cx="12" cy="12" r="10"/></svg>
