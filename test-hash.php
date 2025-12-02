1320616227f3646d6892ed7d20b8ddf3f49328fc
Mohammad Rizki Syahputra <mrizkisyah27@gmail.com>
2025-12-02 11:41:34 +0700

    Remove test-hash

--- test-hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-$plainToken = '7875f98ee3d545640350d95922e2edb5bb2014c2f9a689c2b411ae27ab952adb';
-$hashedToken = hash('sha256', $plainToken);
-echo "Plain token: " . $plainToken . "\n\n";
-echo "Hashed token: " . $hashedToken . "\n\n";
-echo "DB token: b3da05770f4be3c62e1a7ff93b111c1b527ea11ffc916254289ecd793f0e6de8\n\n";
-echo "Match? " . ($hashedToken ? 'YES' : 'NO');
