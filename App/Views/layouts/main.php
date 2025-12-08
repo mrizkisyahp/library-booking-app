cf2b964505c6bc9da72365be6a97ce35c29a1bb3
Daorza <daokun04@gmail.com>
2025-12-08 11:04:48 +0700

    Fikri mencoba tailwind

--- App/Views/layouts/main.php
+++ App/Views/layouts/main.php
@@ -308,17 +308,29 @@ $user = App::$app->user;
             ?>
 
             <!-- beranda -->
-            <a href="<?= $url ?>"
-                class="flex flex-col gap-1.5 text-sm items-center <?= isActiveClass($active, $url) ?>">
+            <a href="<?=  $url?>"
+                class="flex flex-col size-16 gap-1.5 text-sm items-center  <?= isActiveClass($active, $url) ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="lucide lucide-house size-6">
-                    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-                    <path
-                        d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                    class="lucide lucide-home size-6">
+                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                    <polyline points="9 22 9 12 15 12 15 22" />
                 </svg>
                 <span>Beranda</span>
             </a>
+            <!-- <a href="" class="p-2 size-16">
+                <a href="<?= $url ?>"
+                    class="flex flex-col gap-1.5 text-sm items-center <?= isActiveClass($active, $url) ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="lucide lucide-house size-6">
+                        <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                        <path
+                            d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                    </svg>
+                    <span>Beranda</span>
+                </a>
+            </a> -->
 
             <?php if ($user && $user->isAdmin()): ?>
 
--- Script/Seed.php
+++ Script/Seed.php
@@ -47,7 +47,7 @@ try {
 
     $log->info('Role table seeded successfully!');
 
-    $users = [              
+    $users = [
     ['id_user' => 1, 'id_role' => 1, 'nama' => 'admin', 'nip' => '123456789012345678', 'nim' => null, 'email' => 'admin@pnj.ac.id', 'password' => password_hash('admin', PASSWORD_DEFAULT), 'status' => 'active', 'jurusan' => 'Teknik Informatika', 'nomor_hp' => '08123456789', 'suspensi_terakhir' => null, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
     ['id_user' => 2, 'id_role' => 2, 'nama' => 'dosen', 'nip' => '987654321098765432', 'nim' => null, 'email' => 'dosen@pnj.ac.id', 'password' => password_hash('dosen', PASSWORD_DEFAULT), 'status' => 'active', 'jurusan' => 'Teknik Informatika', 'nomor_hp' => '08123456789', 'suspensi_terakhir' => null, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
     ['id_user' => 3, 'id_role' => 3, 'nama' => 'mahasiswa', 'nip' => null, 'nim' => '1234567890', 'email' => 'mahasiswa@stu.pnj.ac.id', 'password' => password_hash('mahasiswa', PASSWORD_DEFAULT), 'status' => 'active', 'jurusan' => 'Teknik Informatika', 'nomor_hp' => '08123456789', 'suspensi_terakhir' => null, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
@@ -124,8 +124,8 @@ try {
     ];
 
     $stmt = $db->pdo->prepare("
-        INSERT INTO ruangan (id_ruangan, nama_ruangan, kapasitas_min, kapasitas_max, jenis_ruangan, deskripsi_ruangan, status_ruangan, created_at, updated_at) 
-        VALUES 
+        INSERT INTO ruangan (id_ruangan, nama_ruangan, kapasitas_min, kapasitas_max, jenis_ruangan, deskripsi_ruangan, status_ruangan, created_at, updated_at)
+        VALUES
         (:id_ruangan, :nama_ruangan, :kapasitas_min, :kapasitas_max, :jenis_ruangan, :deskripsi_ruangan, :status_ruangan, :created_at, :updated_at)
         ON DUPLICATE KEY UPDATE
             id_ruangan = VALUES(id_ruangan),
@@ -144,146 +144,146 @@ try {
 
     $log->info('Ruangan table seeded successfully!');
 
-    // $bookings = [
-    //     ['id_booking' => 1, 'user_id' => 1, 'ruangan_id' => 1, 'tanggal_booking' => '2025-11-05', 'tanggal_penggunaan_ruang' => '2025-11-05', 'waktu_mulai' => '08:00:00', 'waktu_selesai' => '03:00:00', 'tujuan' => 'Meeting', 'status' => 'pending', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_booking' => 2, 'user_id' => 2, 'ruangan_id' => 2, 'tanggal_booking' => '2025-11-05', 'tanggal_penggunaan_ruang' => '2025-11-05', 'waktu_mulai' => '08:00:00', 'waktu_selesai' => '03:00:00', 'tujuan' => 'Meeting', 'status' => 'pending', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_booking' => 3, 'user_id' => 3, 'ruangan_id' => 3, 'tanggal_booking' => '2025-11-05', 'tanggal_penggunaan_ruang' => '2025-11-05', 'waktu_mulai' => '08:00:00', 'waktu_selesai' => '03:00:00', 'tujuan' => 'Meeting', 'status' => 'pending', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    // ];
+    $bookings = [
+        ['id_booking' => 1, 'user_id' => 1, 'ruangan_id' => 1, 'tanggal_booking' => '2025-11-05', 'tanggal_penggunaan_ruang' => '2025-11-05', 'waktu_mulai' => '08:00:00', 'waktu_selesai' => '03:00:00', 'tujuan' => 'Meeting', 'status' => 'pending', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_booking' => 2, 'user_id' => 2, 'ruangan_id' => 2, 'tanggal_booking' => '2025-11-05', 'tanggal_penggunaan_ruang' => '2025-11-05', 'waktu_mulai' => '08:00:00', 'waktu_selesai' => '03:00:00', 'tujuan' => 'Meeting', 'status' => 'pending', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_booking' => 3, 'user_id' => 3, 'ruangan_id' => 3, 'tanggal_booking' => '2025-11-05', 'tanggal_penggunaan_ruang' => '2025-11-05', 'waktu_mulai' => '08:00:00', 'waktu_selesai' => '03:00:00', 'tujuan' => 'Meeting', 'status' => 'pending', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+    ];
 
-    // $stmt = $db->pdo->prepare("
-    //     INSERT INTO booking (id_booking, user_id, ruangan_id, tanggal_booking, tanggal_penggunaan_ruang, waktu_mulai, waktu_selesai, tujuan, status, created_at, updated_at) 
-    //     VALUES 
-    //     (:id_booking, :user_id, :ruangan_id, :tanggal_booking, :tanggal_penggunaan_ruang, :waktu_mulai, :waktu_selesai, :tujuan, :status, :created_at, :updated_at)
-    //     ON DUPLICATE KEY UPDATE
-    //         id_booking = VALUES(id_booking),
-    //         user_id = VALUES(user_id),
-    //         ruangan_id = VALUES(ruangan_id),
-    //         tanggal_booking = VALUES(tanggal_booking),
-    //         tanggal_penggunaan_ruang = VALUES(tanggal_penggunaan_ruang),
-    //         waktu_mulai = VALUES(waktu_mulai),
-    //         waktu_selesai = VALUES(waktu_selesai),
-    //         tujuan = VALUES(tujuan),
-    //         status = VALUES(status),
-    //         updated_at = VALUES(updated_at)
-    // ");
+    $stmt = $db->pdo->prepare("
+        INSERT INTO booking (id_booking, user_id, ruangan_id, tanggal_booking, tanggal_penggunaan_ruang, waktu_mulai, waktu_selesai, tujuan, status, created_at, updated_at)
+        VALUES
+        (:id_booking, :user_id, :ruangan_id, :tanggal_booking, :tanggal_penggunaan_ruang, :waktu_mulai, :waktu_selesai, :tujuan, :status, :created_at, :updated_at)
+        ON DUPLICATE KEY UPDATE
+            id_booking = VALUES(id_booking),
+            user_id = VALUES(user_id),
+            ruangan_id = VALUES(ruangan_id),
+            tanggal_booking = VALUES(tanggal_booking),
+            tanggal_penggunaan_ruang = VALUES(tanggal_penggunaan_ruang),
+            waktu_mulai = VALUES(waktu_mulai),
+            waktu_selesai = VALUES(waktu_selesai),
+            tujuan = VALUES(tujuan),
+            status = VALUES(status),
+            updated_at = VALUES(updated_at)
+    ");
 
-    // foreach ($bookings as $booking) {
-    //     $stmt->execute($booking);
-    // }
+    foreach ($bookings as $booking) {
+        $stmt->execute($booking);
+    }
 
-    // $log->info('Bookings table seeded successfully!');
+    $log->info('Bookings table seeded successfully!');
 
-    // $anggota_bookings = [
-    //     ['id_anggota' => 1, 'booking_id' => 1, 'user_id' => 1, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_anggota' => 2, 'booking_id' => 2, 'user_id' => 2, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_anggota' => 3, 'booking_id' => 3, 'user_id' => 3, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    // ];
+    $anggota_bookings = [
+        ['id_anggota' => 1, 'booking_id' => 1, 'user_id' => 1, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_anggota' => 2, 'booking_id' => 2, 'user_id' => 2, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_anggota' => 3, 'booking_id' => 3, 'user_id' => 3, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+    ];
 
-    // $stmt = $db->pdo->prepare("
-    //     INSERT INTO anggota_booking (id_anggota, booking_id, user_id, created_at, updated_at) 
-    //     VALUES 
-    //     (:id_anggota, :booking_id, :user_id, :created_at, :updated_at)
-    //     ON DUPLICATE KEY UPDATE
-    //         id_anggota = VALUES(id_anggota),
-    //         booking_id = VALUES(booking_id),
-    //         user_id = VALUES(user_id),
-    //         updated_at = VALUES(updated_at)
-    // ");
-    // foreach ($anggota_bookings as $anggota_booking) {
-    //     $stmt->execute($anggota_booking);
-    // }
+    $stmt = $db->pdo->prepare("
+        INSERT INTO anggota_booking (id_anggota, booking_id, user_id, created_at, updated_at)
+        VALUES
+        (:id_anggota, :booking_id, :user_id, :created_at, :updated_at)
+        ON DUPLICATE KEY UPDATE
+            id_anggota = VALUES(id_anggota),
+            booking_id = VALUES(booking_id),
+            user_id = VALUES(user_id),
+            updated_at = VALUES(updated_at)
+    ");
+    foreach ($anggota_bookings as $anggota_booking) {
+        $stmt->execute($anggota_booking);
+    }
 
-    // $log->info('Anggota Booking table seeded successfully!');
+    $log->info('Anggota Booking table seeded successfully!');
 
-    // $feedbacks = [
-    //     ['id_feedback' => 1, 'booking_id' => 1, 'user_id' => 1, 'rating' => 10, 'komentar' => 'wah ruangannya bagus', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_feedback' => 2, 'booking_id' => 2, 'user_id' => 2, 'rating' => 10, 'komentar' => 'wah ruangannya bagus', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_feedback' => 3, 'booking_id' => 3, 'user_id' => 3, 'rating' => 10, 'komentar' => 'wah ruangannya bagus', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    // ];
+    $feedbacks = [
+        ['id_feedback' => 1, 'booking_id' => 1, 'user_id' => 1, 'rating' => 10, 'komentar' => 'wah ruangannya bagus', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_feedback' => 2, 'booking_id' => 2, 'user_id' => 2, 'rating' => 10, 'komentar' => 'wah ruangannya bagus', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_feedback' => 3, 'booking_id' => 3, 'user_id' => 3, 'rating' => 10, 'komentar' => 'wah ruangannya bagus', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+    ];
 
-    // $stmt = $db->pdo->prepare("
-    //     INSERT INTO feedback (id_feedback, booking_id, user_id, rating, komentar, created_at, updated_at) 
-    //     VALUES 
-    //     (:id_feedback, :booking_id, :user_id, :rating, :komentar, :created_at, :updated_at)
-    //     ON DUPLICATE KEY UPDATE
-    //         id_feedback = VALUES(id_feedback),
-    //         booking_id = VALUES(booking_id),
-    //         user_id = VALUES(user_id),
-    //         rating = VALUES(rating),
-    //         komentar = VALUES(komentar),
-    //         updated_at = VALUES(updated_at)
-    // ");
-    // foreach ($feedbacks as $feedback) {
-    //     $stmt->execute($feedback);
-    // }
+    $stmt = $db->pdo->prepare("
+        INSERT INTO feedback (id_feedback, booking_id, user_id, rating, komentar, created_at, updated_at)
+        VALUES
+        (:id_feedback, :booking_id, :user_id, :rating, :komentar, :created_at, :updated_at)
+        ON DUPLICATE KEY UPDATE
+            id_feedback = VALUES(id_feedback),
+            booking_id = VALUES(booking_id),
+            user_id = VALUES(user_id),
+            rating = VALUES(rating),
+            komentar = VALUES(komentar),
+            updated_at = VALUES(updated_at)
+    ");
+    foreach ($feedbacks as $feedback) {
+        $stmt->execute($feedback);
+    }
 
-    // $log->info('Feedback table seeded successfully!');
+    $log->info('Feedback table seeded successfully!');
 
-    // $warnings = [
-    //     ['id_peringatan' => 1, 'nama_peringatan' => 'Terlambat', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_peringatan' => 2, 'nama_peringatan' => 'Cancel by User', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_peringatan' => 3, 'nama_peringatan' => 'Automatic Cancel - Not Enough Users', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    // ];
+    $warnings = [
+        ['id_peringatan' => 1, 'nama_peringatan' => 'Terlambat', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_peringatan' => 2, 'nama_peringatan' => 'Cancel by User', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_peringatan' => 3, 'nama_peringatan' => 'Automatic Cancel - Not Enough Users', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+    ];
 
-    // $stmt = $db->pdo->prepare("
-    //     INSERT INTO peringatan_suspensi (id_peringatan, nama_peringatan, created_at, updated_at) 
-    //     VALUES 
-    //     (:id_peringatan, :nama_peringatan, :created_at, :updated_at)
-    //     ON DUPLICATE KEY UPDATE
-    //         id_peringatan = VALUES(id_peringatan),
-    //         nama_peringatan = VALUES(nama_peringatan),
-    //         updated_at = VALUES(updated_at)
-    // ");
-    // foreach ($warnings as $warning) {
-    //     $stmt->execute($warning);
-    // }
+    $stmt = $db->pdo->prepare("
+        INSERT INTO peringatan_suspensi (id_peringatan, nama_peringatan, created_at, updated_at)
+        VALUES
+        (:id_peringatan, :nama_peringatan, :created_at, :updated_at)
+        ON DUPLICATE KEY UPDATE
+            id_peringatan = VALUES(id_peringatan),
+            nama_peringatan = VALUES(nama_peringatan),
+            updated_at = VALUES(updated_at)
+    ");
+    foreach ($warnings as $warning) {
+        $stmt->execute($warning);
+    }
 
-    // $log->info('Peringatan table seeded successfully!');
+    $log->info('Peringatan table seeded successfully!');
 
-    // $warnings_mahasiswa = [
-    //     ['id_peringatan_mhs' => 1, 'id_akun' => 1, 'id_peringatan' => 1, 'tgl_peringatan' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_peringatan_mhs' => 2, 'id_akun' => 2, 'id_peringatan' => 2, 'tgl_peringatan' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_peringatan_mhs' => 3, 'id_akun' => 3, 'id_peringatan' => 3, 'tgl_peringatan' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    // ];
+    $warnings_mahasiswa = [
+        ['id_peringatan_mhs' => 1, 'id_akun' => 1, 'id_peringatan' => 1, 'tgl_peringatan' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_peringatan_mhs' => 2, 'id_akun' => 2, 'id_peringatan' => 2, 'tgl_peringatan' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_peringatan_mhs' => 3, 'id_akun' => 3, 'id_peringatan' => 3, 'tgl_peringatan' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+    ];
 
-    // $stmt = $db->pdo->prepare("
-    //     INSERT INTO peringatan_mhs (id_peringatan_mhs, id_akun, id_peringatan, tgl_peringatan, created_at, updated_at) 
-    //     VALUES 
-    //     (:id_peringatan_mhs, :id_akun, :id_peringatan, :tgl_peringatan, :created_at, :updated_at)
-    //     ON DUPLICATE KEY UPDATE
-    //         id_peringatan_mhs = VALUES(id_peringatan_mhs),
-    //         id_akun = VALUES(id_akun),
-    //         id_peringatan = VALUES(id_peringatan),
-    //         tgl_peringatan = VALUES(tgl_peringatan),
-    //         updated_at = VALUES(updated_at)
-    // ");
-    // foreach ($warnings_mahasiswa as $warning_mahasiswa) {
-    //     $stmt->execute($warning_mahasiswa);
-    // }
+    $stmt = $db->pdo->prepare("
+        INSERT INTO peringatan_mhs (id_peringatan_mhs, id_akun, id_peringatan, tgl_peringatan, created_at, updated_at)
+        VALUES
+        (:id_peringatan_mhs, :id_akun, :id_peringatan, :tgl_peringatan, :created_at, :updated_at)
+        ON DUPLICATE KEY UPDATE
+            id_peringatan_mhs = VALUES(id_peringatan_mhs),
+            id_akun = VALUES(id_akun),
+            id_peringatan = VALUES(id_peringatan),
+            tgl_peringatan = VALUES(tgl_peringatan),
+            updated_at = VALUES(updated_at)
+    ");
+    foreach ($warnings_mahasiswa as $warning_mahasiswa) {
+        $stmt->execute($warning_mahasiswa);
+    }
 
-    // $log->info('Peringatan Mahasiswa table seeded successfully!');
+    $log->info('Peringatan Mahasiswa table seeded successfully!');
 
-    // $suspends = [
-    //     ['id_suspensi' => 1, 'id_akun' => 1, 'tgl_suspensi' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_suspensi' => 2, 'id_akun' => 2, 'tgl_suspensi' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    //     ['id_suspensi' => 3, 'id_akun' => 3, 'tgl_suspensi' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-    // ];
+    $suspends = [
+        ['id_suspensi' => 1, 'id_akun' => 1, 'tgl_suspensi' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_suspensi' => 2, 'id_akun' => 2, 'tgl_suspensi' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        ['id_suspensi' => 3, 'id_akun' => 3, 'tgl_suspensi' => date('Y-m-d H:i:s'), 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+    ];
 
-    // $stmt = $db->pdo->prepare("
-    //     INSERT INTO suspensi (id_suspensi, id_akun, tgl_suspensi, created_at, updated_at) 
-    //     VALUES 
-    //     (:id_suspensi, :id_akun, :tgl_suspensi, :created_at, :updated_at)
-    //     ON DUPLICATE KEY UPDATE
-    //         id_suspensi = VALUES(id_suspensi),
-    //         id_akun = VALUES(id_akun),
-    //         tgl_suspensi = VALUES(tgl_suspensi),
-    //         updated_at = VALUES(updated_at)
-    // ");
-    // foreach ($suspends as $suspend) {
-    //     $stmt->execute($suspend);
-    // }
+    $stmt = $db->pdo->prepare("
+        INSERT INTO suspensi (id_suspensi, id_akun, tgl_suspensi, created_at, updated_at)
+        VALUES
+        (:id_suspensi, :id_akun, :tgl_suspensi, :created_at, :updated_at)
+        ON DUPLICATE KEY UPDATE
+            id_suspensi = VALUES(id_suspensi),
+            id_akun = VALUES(id_akun),
+            tgl_suspensi = VALUES(tgl_suspensi),
+            updated_at = VALUES(updated_at)
+    ");
+    foreach ($suspends as $suspend) {
+        $stmt->execute($suspend);
+    }
 
-    // $log->info('Suspensi table seeded successfully!');
+    $log->info('Suspensi table seeded successfully!');
 
     $log->info('Seeder completed successfully!');
 } catch (Exception $e) {
